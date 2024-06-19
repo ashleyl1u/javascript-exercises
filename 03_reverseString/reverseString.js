894cef0f73c92103a34557b74b8d69733aac2ce2
Ashley Liu <ashleyl1u095@gmail.com>
2024-06-19 11:23:52 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  if(word.length<=0){
+    return 'ERROR';
+  }
 
+
+  let reverse='';
+
+  for(let i = word.length-1; i>=0;i--){
+    reverse+=word.charAt(i);
+  }
+
+  return reverse;
 };
 
 // Do not edit below this line
