90748c63faa3d2a4742d342d303bdb44f64a2892
Ashley Liu <ashleyl1u095@gmail.com>
2024-06-19 11:17:38 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+  if(num<0){
+    return 'ERROR';
+  }
+  let repeat = '';
+  for(let i =0 ;i<num ;i++){
+    repeat+=word;
+  }
 
+  return repeat;
 };
 
 // Do not edit below this line
