a41dac8e9ae14f86453070688bffaaec43b18389
Ashley Liu <ashleyl1u095@gmail.com>
2024-06-19 11:27:40 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+  let removed = [];
+
+  for(let i =0; i<arr.length ;i++){
+    if(arr[i] !== remove){
+      removed.push(arr[i]);
+    }
+  }
+
+  return removed;
 };
 
 // Do not edit below this line
